0a3f422b23b3196ba1b65ec52b30c6b294ea79f1
Serega163 <sesereregaga@rambler.ru>
2018-12-22 16:20:58 +0400

    Add weather, http-methods, search input and other

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-//import { HttpClient} from '@angular/common/http';
+import { HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -11,10 +11,18 @@ export class AppComponent implements OnInit{
   lat: number = 53.203358;
   lng: number = 50.146048;
 
-  currentLocation: string = "Samara";
-  neededLocation: string = "Moscow";
-  consumption: number = 10;
-  tripPrice: number = 20000;
+  currentLocation: string ;
+  neededLocation: string ;
+  consumption: number = 10;//расход
+  tripPrice: number ;//возврат с сервера
+  searchLocation: string;//строка поиска
+  cityCode: number = 51;//шлется с сервера. ищется по названию города в файле citiesForWeather 
+  weatherPath = "https://clck.yandex.ru/redir/dtype=stred/pid=7/cid=1228/*https://yandex.ru/pogoda/"+this.cityCode;
+  weatherImgPath = "https://info.weather.yandex.net/"+this.cityCode+"/4_white.ru.png?domain=ru";
+  
+  calcUrl = "/data/calcTripPrice";
+  searchUrl = "/data/";
+  getCodeUrl = "/data/";
   
   //constructor(private http: HttpClient){}
   
@@ -22,14 +30,25 @@ export class AppComponent implements OnInit{
 	  console.log("cL"+this.currentLocation);
   }
   
-  calc(){
-	  console.log("cL"+this.currentLocation);
-	 /* this.http.get({"current": currentLocation, "needed": neededLocation, "consumption": consumption})
-	  .subscribe((data) => this.tripPrice=data);*/
-	 /* this.service.simpleGet('data', '/getConsumtion', {current:currentLocation,needed:neededLocation }).subscribe(response => { 
-		const data = response.data; 
-		tripPrice: data.price;
-		}); */
-}
+ getCityCode(){//возврат кода города для информера
+		let object = {neededLocation: this.neededLocation}
+		this.http.post(this.getCodeUrl, JSON.stringify(object)
+		).subscribe((data)=>{this.cityCode = data})
+  }
+  
+  calc(){// отправка расхода, принятие  стоимости
+		let object = {current: this.currentLocation, needed: this.neededLocation, consumption: this.consumption}
+		this.http.post(this.calcUrl, JSON.stringify(object)
+		).subscribe((data)=>{this.tripPrice = data})
+	}
+	
+	search(){// отрправка города, получение координат
+		let object = {search: this.searchLocation}
+		this.http.post(this.searchUrl, JSON.stringify(object)
+		).subscribe((data) => {
+			this.lat=data.lat; 
+			this.lng=data.lng
+			})
+	}
   }
   
